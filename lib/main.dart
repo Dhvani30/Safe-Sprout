feeadfc5e2c98381e0a796763dd9cc5d927d7951
Dhvani.30 <dhvani30.mistry@gmail.com>
2026-01-18 17:19:57 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,52 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'gradient_container.dart';
+// import 'firebase_options.dart';
+// import 'modules/home_page.dart'; // Import your home page
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+//   // Check if the user is already authenticated
+//   User? user = FirebaseAuth.instance.currentUser;
+
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Your App Name',
+//       home: user != null
+//           ?
+//            const HomePage()
+//           : const Scaffold(
+//               backgroundColor: Color.fromARGB(255, 157, 129, 137),
+//               body: GradientContainer(
+//                 Color.fromARGB(255, 201, 178, 239),
+//                 // Color.fromARGB(255, 255, 255, 255),
+//                 Color.fromARGB(255, 212, 214, 249),
+//                 Color.fromARGB(255, 233, 212, 232),
+//               ),
+//             ),
+//     ),
+//   );
+// }
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'gradient_container.dart';
 import 'firebase_options.dart';
 import 'modules/home_page.dart'; // Import your home page
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // Check if the user is already authenticated
-  User? user = FirebaseAuth.instance.currentUser;
-
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Your App Name',
-      home: user != null
-          ? const HomePage()
-          : const Scaffold(
-              backgroundColor: Color.fromARGB(255, 157, 129, 137),
-              body: GradientContainer(
-                Color.fromARGB(255, 201, 178, 239),
-                // Color.fromARGB(255, 255, 255, 255),
-                Color.fromARGB(255, 212, 214, 249),
-                Color.fromARGB(255, 233, 212, 232),
-              ),
-            ),
+      home: const HomePage(), // Directly set HomePage as the first page
     ),
   );
 }
